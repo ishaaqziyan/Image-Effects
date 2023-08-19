61583caf258d238fc875a8c00cc7abf63181b0b9
Ishaaq Ziyan <ishaaqziyan@gmail.com>
2023-08-19 08:24:55 +0530

    Initated Development

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './public/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist' ),
+        filename: 'index.js'
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+    
+}
